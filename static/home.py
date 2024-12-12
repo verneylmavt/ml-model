7c716ff01596f85039697f02794dfc132c869dfe
Elvern Neylmav Tanny <elvernneylmav@gmail.com>
2024-12-12 23:07:30 +0800

    update

--- static/home.py
+++ static/home.py
@@ -1,15 +1,20 @@
 import streamlit as st
 
 def run():
-    st.title("Welcome to My Machine Learning Models Collection")
-    st.write("""
-        ## Overview
-        This website is a collection of machine learning models developed by **Elvern Neylmav T**. 
-        Each page represents a different model with its unique functionalities and capabilities.
-
-        ## How to Use
-        Navigate to the **Models** section using the sidebar to access and interact with each model.
-    """)
-    # st.image("https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png", width=300)
-
+    st.title("Welcome to My ML Models Collection")
+    st.write(
+        """
+        This application hosts a collection of machine learning models, allowing you to explore their functionality interactively.
+        """
+    )
+    st.markdown(
+        """
+        **If you encounter any issues or errors**, please check the repository for troubleshooting:
+        [GitHub Repository](https://github.com/verneylmavt/ml-model)
+        """
+    )
+    st.write("Feel free to fork or clone the repository, install the requirements, and run the app locally if needed.")
+    st.divider()
+    st.info("Navigate through the app to explore various models and their features.")
+    
 run()
